81a82b5cdf209083cee6048b9d8982d98223d920
Kirill <apatski94@gmail.com>
2023-05-17 01:10:07 +0300

    create store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,17 @@
-export {};
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { authReducer } from 'store/slices/authSlice'
+import thunkMiddleware from 'redux-thunk'
+import { appReducer } from 'store/slices/appSlice'
+
+const rootReducer = combineReducers({
+  app: appReducer,
+  auth: authReducer
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
