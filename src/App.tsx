8ffe79cd4e5a21cff7b2f40b65b0ccb4aa093af2
Kirill <apatski94@gmail.com>
2023-05-17 01:11:03 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import 'styles/App.css'
+import React, { useEffect } from 'react'
+import 'styles/App.scss'
 import { Header } from 'components/layout/Header/Header'
 import { Pages } from 'pages/Pages'
+import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
+import { authMe } from 'store/slices/authSlice'
+import { getIsInitialized } from 'store/selectors/appSelectors'
+import { AppLoader } from 'components/AppLoader/AppLoader'
 
 export const App = () => {
+  const dispatch = useAppDispatch()
+  const isInitialized = useAppSelector(getIsInitialized)
+  useEffect(() => {
+    dispatch(authMe())
+  }, [])
+  if (isInitialized) {
+    return <AppLoader />
+  }
   return (
     <>
       <Header />
