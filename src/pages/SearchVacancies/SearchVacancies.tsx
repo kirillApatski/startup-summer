6bb42fc91a8bbe4730765d07012acd066be80613
Kirill <apatski94@gmail.com>
2023-05-16 12:57:03 +0300

    added SearchVacancies and style

--- src/pages/SearchVacancies/SearchVacancies.tsx
+++ src/pages/SearchVacancies/SearchVacancies.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styles from './SearchVacancies.module.scss'
+import { Filters } from 'pages/SearchVacancies/Filters/Filters'
 
 export const SearchVacancies = () => {
-  return <div>SearchVacancies</div>
+  return (
+    <div className={styles.wrapper}>
+      <Filters />
+    </div>
+  )
 }
