c5b2fe61201a3908a6998d1d04563553ebd1cf71
Kirill <apatski94@gmail.com>
2023-05-18 00:08:23 +0300

    rename

--- src/components/Vacancy/Vacancy.tsx
+++ src/components/Vacancy/Vacancy.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { ActionIcon } from '@mantine/core'
 import { DotIcon } from 'assets/icons/DotIcon'
 import { LocationIcon } from 'assets/icons/LocationIcon'
@@ -6,22 +6,40 @@ import styles from 'components/Vacancy/Vacancy.module.scss'
 import { StarIcon } from 'assets/icons/StarIcon'
 import { NavLink } from 'react-router-dom'
 import { PATH } from 'common/enums/PATH'
+import { salaryFork } from 'utils/getSalatyFork'
 
-export const Vacancy = () => {
+type VacancyPropsType = {
+  profession: string
+  payment_from: number
+  payment_to: number
+  type_of_work: { id: number; title: string }
+  town: { id: number; title: string }
+  currency: string
+}
+
+export const Vacancy: FC<VacancyPropsType> = ({
+  profession,
+  type_of_work,
+  town,
+  payment_to,
+  payment_from,
+  currency
+}) => {
+  const salary = salaryFork(payment_from, payment_to, currency)
   return (
     <div className={styles.vacancyWrapper}>
       <div className={styles.descriptionVacancy}>
         <NavLink to={`${PATH.VACANCY_DETAILS}/123`}>
-          <h2 className={styles.title}>Менеджер-дизайнер</h2>
+          <h2 className={styles.title}>{profession}</h2>
         </NavLink>
         <p className={styles.descriptionWork}>
-          <span className={styles.salary}>з/п от 70000 rub</span>
+          <span className={styles.salary}>{salary}</span>
           <DotIcon />
-          <span className={styles.timeWork}>Полный рабочий день</span>
+          <span className={styles.timeWork}>{type_of_work.title}</span>
         </p>
         <p className={styles.city}>
           <LocationIcon />
-          <span>Новый Уренгой</span>
+          <span>{town.title}</span>
         </p>
       </div>
       <ActionIcon className={styles.star}>
---
+++ src/pages/vacancies/JobBoard/JobBoard.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import { Vacancy } from 'components/Vacancy/Vacancy'
+import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
+import { getVacanciesState } from 'store/selectors/VacanciesSelectors'
+import { getVacancies } from 'store/slices/vacanciesSlice'
+
+export const JobBoard = () => {
+  const dispatch = useAppDispatch()
+  const vacancies = useAppSelector(getVacanciesState)
+  useEffect(() => {
+    dispatch(getVacancies(1))
+  }, [])
+  return (
+    <>
+      {vacancies.map(vacancy => {
+        return (
+          <Vacancy
+            key={vacancy.id}
+            profession={vacancy.profession}
+            payment_from={vacancy.payment_from}
+            payment_to={vacancy.payment_to}
+            type_of_work={vacancy.type_of_work}
+            town={vacancy.town}
+            currency={vacancy.currency}
+          />
+        )
+      })}
+    </>
+  )
+}
