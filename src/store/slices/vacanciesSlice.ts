b0495338cb2a85ca0918351448a8b63c6ae88e74
Kirill <apatski94@gmail.com>
2023-05-18 12:45:13 +0300

    add functions setPage / setFilters

--- src/store/slices/vacanciesSlice.ts
+++ src/store/slices/vacanciesSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { createAppAsyncThunk } from 'utils/create-app-async-thunk'
 import { FilterAndSearchType, vacanciesApi, VacanciesResponseType } from 'api/vacanciesApi'
 
@@ -6,31 +6,43 @@ const initialState: VacanciesResponseType & FilterAndSearchType = {
   objects: [],
   total: 0,
   count: 4,
-  filterAndSearch: {
-    published: 1,
-    keyword: null,
-    payment_from: null,
-    payment_to: null,
-    catalogues: null
-  }
+  published: 1,
+  page: null,
+  keyword: null,
+  payment_from: null,
+  payment_to: null,
+  catalogues: null
 }
 
-export const getVacancies = createAppAsyncThunk(
-  'vacancies/getVacancies',
-  async (currenPage: number, { rejectWithValue }) => {
-    try {
-      const res = await vacanciesApi.getVacancies(currenPage)
-      return res.data
-    } catch (error) {
-      return rejectWithValue(error)
-    }
+export const getVacancies = createAppAsyncThunk('vacancies/getVacancies', async (_, { rejectWithValue, getState }) => {
+  try {
+    const { count, published, page, keyword, payment_to, payment_from, catalogues } = getState().vacancies
+    const res = await vacanciesApi.getVacancies({
+      count,
+      page,
+      published,
+      keyword,
+      payment_to,
+      payment_from,
+      catalogues
+    })
+    return res.data
+  } catch (error) {
+    return rejectWithValue(error)
   }
-)
+})
 
 const vacanciesSlice = createSlice({
   name: 'vacancies',
   initialState,
-  reducers: {},
+  reducers: {
+    setPage(state, action: PayloadAction<number>) {
+      state.page = action.payload
+    },
+    setFilters(state, action: PayloadAction<Partial<FilterAndSearchType>>) {
+      state.keyword = action.payload.keyword
+    }
+  },
   extraReducers: builder => {
     builder.addCase(getVacancies.fulfilled, (state, action) => {
       state.objects = action.payload.objects
@@ -40,3 +52,4 @@ const vacanciesSlice = createSlice({
 })
 
 export const vacanciesReducer = vacanciesSlice.reducer
+export const { setPage, setFilters } = vacanciesSlice.actions
