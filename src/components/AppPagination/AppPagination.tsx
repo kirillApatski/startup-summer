92a012cafb6069ad58ac759cfcf0f6cf18cfe006
Kirill <apatski94@gmail.com>
2023-05-18 12:42:09 +0300

    refactoring pagination

--- src/components/AppPagination/AppPagination.tsx
+++ src/components/AppPagination/AppPagination.tsx
@@ -1,19 +1,29 @@
 import React, { FC } from 'react'
 import { Pagination } from '@mantine/core'
+import { getVacancies, setPage } from 'store/slices/vacanciesSlice'
+import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
 
 type AppPaginationType = {
   className?: string
   total: number
-  getNewPageVacancies: (page: number) => void
 }
 
-const AppPagination: FC<AppPaginationType> = ({ className, total, getNewPageVacancies }) => {
-  const finishStyles = className ? className : ''
+const AppPagination: FC<AppPaginationType> = ({ className, total }) => {
+  const dispatch = useAppDispatch()
+
+  const currentPage = useAppSelector(state => state.vacancies.page)
+
   const pageQuantity = Math.ceil((total > 500 ? 500 : total) / 4)
-  const currentPageHandler = (currenPage: number) => {
-    getNewPageVacancies(currenPage)
+
+  const currentPageHandler = (page: number) => {
+    dispatch(setPage(page - 1))
+    dispatch(getVacancies())
   }
-  return <Pagination className={finishStyles} total={pageQuantity} onChange={currentPageHandler} />
+
+  const finishStyles = className ? className : ''
+  return (
+    <Pagination className={finishStyles} total={pageQuantity} value={currentPage! + 1} onChange={currentPageHandler} />
+  )
 }
 
 export default AppPagination
