6de7e8f9f5d7217f432aa577264e4b108b89f059
Kirill <apatski94@gmail.com>
2023-05-18 12:44:11 +0300

    add logic searchInput search vacancies

--- src/pages/vacancies/SearchInput/SearchInput.tsx
+++ src/pages/vacancies/SearchInput/SearchInput.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
 import { Button, Input } from '@mantine/core'
 import { SearchIcon } from 'assets/icons/SearchIcon'
 import styles from './SearchInput.module.scss'
+import { useAppDispatch } from 'common/hooks/hooks'
+import { getVacancies, setFilters } from 'store/slices/vacanciesSlice'
 
 export const SearchInput = () => {
+  const dispatch = useAppDispatch()
+  const [inputValue, setInputValue] = useState('')
+
+  const onSearchVacancies = () => {
+    dispatch(setFilters({ keyword: inputValue }))
+    dispatch(getVacancies())
+  }
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.currentTarget.value)
+  }
+  const onKeyDownEnterHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    event.key === 'Enter' && onSearchVacancies()
+  }
   return (
     <Input
       className={styles.input}
+      value={inputValue}
+      onChange={onChangeHandler}
+      onKeyDown={onKeyDownEnterHandler}
       icon={<SearchIcon />}
       placeholder='Введите название вакансии'
       size={'md'}
       rightSectionWidth={105}
-      rightSection={<Button className={styles.searchButton}>Поиск</Button>}
+      rightSection={
+        <Button className={styles.searchButton} onClick={onSearchVacancies}>
+          Поиск
+        </Button>
+      }
     />
   )
 }
