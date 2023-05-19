7b5937e94d0160b41b32660b0e23db682d461b66
Kirill <apatski94@gmail.com>
2023-05-19 18:18:32 +0300

    add styles and add new Props

--- src/pages/vacancies/JobBoard/JobBoard.tsx
+++ src/pages/vacancies/JobBoard/JobBoard.tsx
@@ -3,6 +3,7 @@ import { Vacancy } from 'components/Vacancy/Vacancy'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
 import { getVacanciesState } from 'store/selectors/VacanciesSelectors'
 import { getVacancies } from 'store/slices/vacanciesSlice'
+import styles from './JobBoard.module.scss'
 
 export const JobBoard = () => {
   const dispatch = useAppDispatch()
@@ -15,7 +16,10 @@ export const JobBoard = () => {
       {vacancies.map(vacancy => {
         return (
           <Vacancy
+            styles={styles}
+            data-elem={`vacancy-_vacancy-${vacancy.id}`}
             key={vacancy.id}
+            id={vacancy.id}
             profession={vacancy.profession}
             payment_from={vacancy.payment_from}
             payment_to={vacancy.payment_to}
