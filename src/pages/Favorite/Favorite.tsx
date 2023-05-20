7592955af3893b1363a37cbfcbac00608f624b08
Kirill <apatski94@gmail.com>
2023-05-20 15:22:29 +0300

    add logic get item from local storage and add styles (test)

--- src/pages/Favorite/Favorite.tsx
+++ src/pages/Favorite/Favorite.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { VacancyType } from 'api/vacanciesApi'
+import { Vacancy } from 'components/Vacancy/Vacancy'
+import styles from './Favorite.module.scss'
 
 export const Favorite = () => {
-  return <div>Favorite</div>
+  const favoriteData: VacancyType[] = JSON.parse(localStorage.getItem('favorite') || '')
+
+  return (
+    <div className={styles.favoriteWrapper}>
+      {favoriteData.map(el => {
+        return <Vacancy key={el.id} vacancy={el} />
+      })}
+    </div>
+  )
 }
