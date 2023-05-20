3c0bcebe4688ac6121803d9ae952e5708456d2c0
Kirill <apatski94@gmail.com>
2023-05-20 15:19:24 +0300

    refactoring props and styles

--- src/pages/VacancyDetails/VacancyDetails.tsx
+++ src/pages/VacancyDetails/VacancyDetails.tsx
@@ -7,24 +7,18 @@ import styles from './VacancyDetails.module.scss'
 
 export const VacancyDetails = () => {
   const { id } = useParams()
+
   let vacancy = useAppSelector(state => state.vacancies.objects.find(vacancy => vacancy.id === Number(id)))
   vacancy = vacancy ? vacancy : getDataToLocalStorage('vacancy')
 
   useEffect(() => {
     setDataToLocalStorage('vacancy', JSON.stringify(vacancy))
   }, [vacancy])
+
   return (
     <div className={styles.wrapper}>
-      <Vacancy
-        styles={styles}
-        profession={vacancy!.profession}
-        payment_from={vacancy!.payment_from}
-        payment_to={vacancy!.payment_to}
-        type_of_work={vacancy!.type_of_work}
-        town={vacancy!.town}
-        currency={vacancy!.currency}
-        id={vacancy!.id}
-      />
+      <Vacancy styles={styles} vacancy={vacancy!} />
+      <div className={styles.vacancyDescription} dangerouslySetInnerHTML={{ __html: vacancy!.vacancyRichText }}></div>
     </div>
   )
 }
