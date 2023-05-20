896db08f09479cba7640c8d31c91f4985537a48c
Kirill <apatski94@gmail.com>
2023-05-20 15:20:11 +0300

    add test logic set to localStorage and refactoring styles

--- src/pages/vacancies/JobBoard/JobBoard.tsx
+++ src/pages/vacancies/JobBoard/JobBoard.tsx
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Vacancy } from 'components/Vacancy/Vacancy'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
 import { getVacanciesState } from 'store/selectors/VacanciesSelectors'
 import { getVacancies } from 'store/slices/vacanciesSlice'
-import styles from './JobBoard.module.scss'
+import { VacancyType } from 'api/vacanciesApi'
+import { setDataToLocalStorage } from 'utils/localStorage'
 
 export const JobBoard = () => {
   const dispatch = useAppDispatch()
   const vacancies = useAppSelector(getVacanciesState)
+
+  const [favoriteDta, setFavoriteDta] = useState<VacancyType[]>([])
+
+  const setItemToLocalStorage = (e: MouseEvent, vacancy: VacancyType) => {
+    e.preventDefault()
+    setFavoriteDta([...favoriteDta, vacancy])
+  }
+  useEffect(() => {
+    setDataToLocalStorage('favorite', JSON.stringify(favoriteDta))
+  }, [favoriteDta])
+
   useEffect(() => {
     dispatch(getVacancies())
   }, [dispatch])
@@ -16,16 +28,10 @@ export const JobBoard = () => {
       {vacancies.map(vacancy => {
         return (
           <Vacancy
-            styles={styles}
-            data-elem={`vacancy-_vacancy-${vacancy.id}`}
             key={vacancy.id}
-            id={vacancy.id}
-            profession={vacancy.profession}
-            payment_from={vacancy.payment_from}
-            payment_to={vacancy.payment_to}
-            type_of_work={vacancy.type_of_work}
-            town={vacancy.town}
-            currency={vacancy.currency}
+            data-elem={`vacancy-_vacancy-${vacancy.id}`}
+            vacancy={vacancy}
+            setItemToLocalStorage={setItemToLocalStorage}
           />
         )
       })}
