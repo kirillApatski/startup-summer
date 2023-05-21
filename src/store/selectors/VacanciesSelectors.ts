fa44eb0ddee988fbdc54fbe7e4e8516448bd6dae
Kirill <apatski94@gmail.com>
2023-05-22 02:13:28 +0300

    delete comment and refactoring style

--- src/store/selectors/VacanciesSelectors.ts
+++ src/store/selectors/VacanciesSelectors.ts
@@ -3,6 +3,3 @@ import { VacancyType } from 'api/vacanciesApi'
 
 export const getVacanciesState = (state: RootState): VacancyType[] => state.vacancies.objects
 export const getTotalCount = (state: RootState): number => state.vacancies.total
-// export const getVacancy = (state: RootState, id: number): VacancyType | undefined => {
-//   return state.vacancies.objects.find(vacancy => vacancy.id === id)
-// }
