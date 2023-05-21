c38e5502e704d0d37ed050582b5eab6c86db4e70
Kirill <apatski94@gmail.com>
2023-05-22 02:15:30 +0300

    add test start loading page favorite

--- src/pages/Favorite/Favorite.tsx
+++ src/pages/Favorite/Favorite.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { VacancyType } from 'api/vacanciesApi'
 import { Vacancy } from 'components/Vacancy/Vacancy'
 import styles from './Favorite.module.scss'
+import { useAppSelector } from 'common/hooks/hooks'
+import { getFavorite } from 'store/selectors/favoriteSelectors'
 
 export const Favorite = () => {
   const favoriteData: VacancyType[] = JSON.parse(localStorage.getItem('favorite') || '')
+  const data = useAppSelector(getFavorite)
+  let a = favoriteData ? favoriteData : data
 
   return (
     <div className={styles.favoriteWrapper}>
-      {favoriteData.map(el => {
+      {a.map(el => {
         return <Vacancy key={el.id} vacancy={el} />
       })}
     </div>
