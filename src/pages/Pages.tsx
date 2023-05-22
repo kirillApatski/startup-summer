9badced5e7def633bd99f4c80caaa42bcef8b56a
Kirill <apatski94@gmail.com>
2023-05-22 18:50:44 +0300

    add route to empty-state

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { PATH } from 'common/enums/PATH'
-import { Vacancies } from 'pages/vacancies/Vacancies'
 import { Favorite } from 'pages/Favorite/Favorite'
 import { VacancyDetails } from 'pages/VacancyDetails/VacancyDetails'
+import { EmptyState } from 'pages/EmptyState/EmptyState'
+import { Vacancies } from 'pages/vacancies/Vacancies'
 
 export const Pages = () => {
   return (
@@ -13,6 +14,7 @@ export const Pages = () => {
         <Route path={PATH.SEARCH_VACANCIES} element={<Vacancies />} />
         <Route path={`${PATH.SEARCH_VACANCIES}/:id`} element={<VacancyDetails />} />
         <Route path={PATH.FAVORITE} element={<Favorite />} />
+        <Route path={PATH.EMPTY_STATE} element={<EmptyState />} />
         <Route path='*' element={<Navigate to={PATH.SEARCH_VACANCIES} />} />
       </Routes>
     </div>
