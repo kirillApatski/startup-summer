3e4f80aff846b04dac36ff5f06e5b5c15f29a872
Kirill <apatski94@gmail.com>
2023-05-23 00:32:51 +0300

    add style and add loader

--- src/pages/vacancies/JobBoard/JobBoard.tsx
+++ src/pages/vacancies/JobBoard/JobBoard.tsx
@@ -3,19 +3,29 @@ import { Vacancy } from 'components/Vacancy/Vacancy'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
 import { getVacanciesState } from 'store/selectors/VacanciesSelectors'
 import { getVacancies } from 'store/slices/vacanciesSlice'
+import styles from './JobBoard.module.scss'
+import { AppLoader } from 'components/AppLoader/AppLoader'
 
 export const JobBoard = () => {
   const dispatch = useAppDispatch()
   const vacancies = useAppSelector(getVacanciesState)
+  const isLoading = useAppSelector(state => state.app.status === 'loading')
 
   useEffect(() => {
     dispatch(getVacancies())
   }, [dispatch])
+
   return (
-    <>
-      {vacancies.map(vacancy => {
-        return <Vacancy isLink={true} key={vacancy.id} data-elem={`vacancy-_vacancy-${vacancy.id}`} vacancy={vacancy} />
-      })}
-    </>
+    <div className={styles.wrapper}>
+      {isLoading ? (
+        <AppLoader />
+      ) : (
+        vacancies.map(vacancy => {
+          return (
+            <Vacancy isLink={true} key={vacancy.id} data-elem={`vacancy-_vacancy-${vacancy.id}`} vacancy={vacancy} />
+          )
+        })
+      )}
+    </div>
   )
 }
