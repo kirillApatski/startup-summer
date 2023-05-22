cf980d51194340f03cfc9f77153f6634e1dac029
Kirill <apatski94@gmail.com>
2023-05-22 18:47:22 +0300

    added hook usePagination and NavLinkContainer component

---
+++ src/common/hoc/NavLinkContainer.tsx
@@ -0,0 +1,24 @@
+import React, { FC, ReactNode } from 'react'
+import { NavLink } from 'react-router-dom'
+import { PATH } from 'common/enums/PATH'
+
+type NavLinkContainerPropsType = {
+  children: ReactNode
+  isLink: boolean
+  className: any
+  vacancyId: number
+}
+
+export const NavLinkContainer: FC<NavLinkContainerPropsType> = ({ children, isLink, className, vacancyId }) => {
+  return (
+    <>
+      {isLink ? (
+        <NavLink to={`${PATH.VACANCY_DETAILS}/${vacancyId}`} className={className}>
+          {children}
+        </NavLink>
+      ) : (
+        <div className={className}>{children}</div>
+      )}
+    </>
+  )
+}
---
+++ src/common/hooks/usePagination.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { VacancyType } from 'api/vacanciesApi'
+
+export const usePagination = (count: number, finalDataFavoriteVacancy: VacancyType[]) => {
+  const [page, setPage] = useState<number>(1)
+  const pageCount = Math.ceil(finalDataFavoriteVacancy.length / count)
+  const lastContentIndex = page * count
+  const firstContentIndex = lastContentIndex - count
+  if (page > pageCount) {
+    setPage(state => state - 1)
+  }
+  const setPageHandler = (pageNumber: number) => {
+    setPage(pageNumber)
+  }
+
+  return {
+    setPageHandler,
+    totalPage: pageCount,
+    firstContentIndex,
+    lastContentIndex,
+    page
+  }
+}
