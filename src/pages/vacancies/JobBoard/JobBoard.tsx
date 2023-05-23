0d0d69e88045c9a32481107c9883ec58e81c046a
Kirill <apatski94@gmail.com>
2023-05-23 15:22:02 +0300

    add navigate

--- src/pages/vacancies/JobBoard/JobBoard.tsx
+++ src/pages/vacancies/JobBoard/JobBoard.tsx
@@ -1,20 +1,28 @@
 import React, { useEffect } from 'react'
 import { Vacancy } from 'components/Vacancy/Vacancy'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
-import { getVacanciesState } from 'store/selectors/VacanciesSelectors'
+import { getTotalCount, getVacanciesState } from 'store/selectors/VacanciesSelectors'
 import { getVacancies } from 'store/slices/vacanciesSlice'
 import styles from './JobBoard.module.scss'
 import { AppLoader } from 'components/AppLoader/AppLoader'
+import { useNavigate } from 'react-router-dom'
+import { PATH } from 'common/enums/PATH'
 
 export const JobBoard = () => {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
   const vacancies = useAppSelector(getVacanciesState)
   const isLoading = useAppSelector(state => state.app.status === 'loading')
+  const totalCount = useAppSelector(getTotalCount)
 
   useEffect(() => {
     dispatch(getVacancies())
   }, [dispatch])
+
+  if (totalCount === 0) {
+    navigate(PATH.EMPTY_STATE)
+  }
 
   return (
     <div className={styles.wrapper}>
