0c1fd945b4cf3126137c0391c434b91ef54d7444
Kirill <apatski94@gmail.com>
2023-05-23 15:20:56 +0300

    refactoring magic number

--- src/components/AppPagination/AppPagination.tsx
+++ src/components/AppPagination/AppPagination.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { Pagination } from '@mantine/core'
 import { getVacancies, setPage } from 'store/slices/vacanciesSlice'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
+import { defaultCountPage, maxTotalItemApp } from 'common/constants/constants'
 
 type AppPaginationType = {
   className?: string
@@ -13,7 +14,7 @@ const AppPagination: FC<AppPaginationType> = ({ className, total }) => {
 
   const currentPage = useAppSelector(state => state.vacancies.pagination.page)
 
-  const pageQuantity = Math.ceil((total > 500 ? 500 : total) / 4)
+  const pageQuantity = Math.ceil((total > maxTotalItemApp ? maxTotalItemApp : total) / defaultCountPage)
 
   const currentPageHandler = (page: number) => {
     dispatch(setPage(page - 1))
@@ -23,7 +24,7 @@ const AppPagination: FC<AppPaginationType> = ({ className, total }) => {
   const finishStyles = className ? className : ''
 
   return (
-    <Pagination className={finishStyles} total={pageQuantity} value={currentPage! + 1} onChange={currentPageHandler} />
+    <Pagination className={finishStyles} total={pageQuantity!} value={currentPage! + 1} onChange={currentPageHandler} />
   )
 }
 
