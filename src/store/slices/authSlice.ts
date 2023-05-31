3e6b90449da9633c3eb7a9dc81e6a7bc4e0214b4
Kirill <apatski94@gmail.com>
2023-05-31 09:47:37 +0300

    fix ttl

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -22,7 +22,7 @@ export const authMe = createAppAsyncThunk('auth/authMe', async (_, { dispatch, r
       const res = await authApi.passwordAuth()
       dispatch(setAuthData(res.data))
       setDataToLocalStorage('auth', JSON.stringify(res.data))
-    } else if (ttl < Date.now()) {
+    } else if (ttl * 1000 < Date.now()) {
       dispatch(setIsInitialized({ isInitialized: true }))
       dispatch(authRefreshToken(refreshToken))
     }
